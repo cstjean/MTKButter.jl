c4d5832f5c8f00807a806569c76d796ef5591b56
cstjean <st-jean.cb@om.asahi-kasei.co.jp>
2025-07-18 12:03:30 +0900

    name-kwarg piracy

--- src/MTKButter.jl
+++ src/MTKButter.jl
@@ -6,4 +6,10 @@ using Accessors, MLStyle, OrderedCollections
 
 include("bmodel.jl")
 
+function __init__()
+    # PIRACY! But it should be benign.
+    # I'd like to use `nothing`, but MTK doesn't like it
+    @eval MTK (m::Model)(args...; name=:unspecified, kw...) = m.f(args...; name, kw...)
+end
+
 end
