5de4f128de450e3c782b55cfe51ac61786796410
cstjean <st-jean.cb@om.asahi-kasei.co.jp>
2025-07-21 00:20:12 +0900

    unspecified -> unnamed

--- src/MTKButter.jl
+++ src/MTKButter.jl
@@ -10,7 +10,7 @@ include("bmodel.jl")
 function __init__()
     # PIRACY! But it should be benign.
     # I'd like to use `nothing`, but MTK doesn't like it
-    @eval MTK (m::Model)(args...; name=:unspecified, kw...) = m.f(args...; name, kw...)
+    @eval MTK (m::Model)(args...; name=:unnamed, kw...) = m.f(args...; name, kw...)
 end
 
 end
