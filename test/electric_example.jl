8f755c30012d4e47259bd4fe55b1b80a02a9d986
cstjean <st-jean.cb@om.asahi-kasei.co.jp>
2025-07-21 12:53:39 -0400

    Add test/electric_example (currently broken)

---
+++ test/electric_example.jl
@@ -0,0 +1,87 @@
+using ModelingToolkit, Plots, OrdinaryDiffEq
+using ModelingToolkit: t_nounits as t, D_nounits as D
+using MTKButter
+
+@connector Pin begin
+    v(t)
+    i(t), [connect = Flow]
+end
+
+@mtkbmodel Ground begin
+    @components begin
+        g = Pin()
+    end
+    @equations begin
+        g.v ~ 0
+    end
+end
+
+@mtkbmodel OnePort begin
+    @components begin
+        p = Pin()
+        n = Pin()
+    end
+    @variables begin
+        v(t)
+        i(t)
+    end
+    @equations begin
+        v ~ p.v - n.v
+        0 ~ p.i + n.i
+        i ~ p.i
+    end
+end
+
+@mtkbmodel Resistor begin
+    @extend OnePort()
+    @parameters begin
+        R = 1.0 # Sets the default resistance
+    end
+    @equations begin
+        v ~ i * R
+    end
+end
+
+@mtkbmodel Capacitor begin
+    @extend OnePort()
+    @parameters begin
+        C = 1.0
+    end
+    @equations begin
+        D(v) ~ i / C
+    end
+end
+
+@mtkbmodel ConstantVoltage begin
+    @extend OnePort()
+    @parameters begin
+        V = 1.0
+    end
+    @equations begin
+        V ~ v
+    end
+end
+
+@mtkbmodel RCModel begin
+    @description "A circuit with a constant voltage source, resistor and capacitor connected in series."
+    @components begin
+        resistor = Resistor(R = 1.0)
+        capacitor = Capacitor(C = 1.0)
+        source = ConstantVoltage(V = 1.0)
+        ground = Ground()
+    end
+    @equations begin
+        connect(source.p, resistor.p)
+        connect(resistor.n, capacitor.p)
+        connect(capacitor.n, source.n)
+        connect(capacitor.n, ground.g)
+    end
+end
+
+@mtkcompile rc_model = RCModel(resistor.R = 2.0)
+u0 = [
+    rc_model.capacitor.v => 0.0
+]
+prob = ODEProblem(rc_model, u0, (0, 10.0))
+sol = solve(prob)
+plot(sol)
--- test/runtests.jl
+++ test/runtests.jl
@@ -10,11 +10,10 @@ The file will be automatically included inside a `@testset` with title "Title Fo
 =#
 for (root, dirs, files) in walkdir(@__DIR__)
     for file in files
-        if isnothing(match(r"^test-.*\.jl$", file))
+        if !endswith(file, ".jl")
             continue
         end
-        title = titlecase(replace(splitext(file[6:end])[1], "-" => " "))
-        @testset "$title" begin
+        @testset "$file" begin
             include(file)
         end
     end
