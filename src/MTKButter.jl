17417664ce1e774c3e6303ef3a68d6c072cd0df8
cstjean <st-jean.cb@om.asahi-kasei.co.jp>
2025-07-21 12:21:28 -0400

    Disable precompilation

--- src/MTKButter.jl
+++ src/MTKButter.jl
@@ -1,3 +1,4 @@
+__precompile__(false)  # apparently even the __init__ piracy is problematic
 module MTKButter
 
 using ModelingToolkit, MacroTools
@@ -7,10 +8,13 @@ using MacroTools: postwalk
 
 include("bmodel.jl")
 
-function __init__()
+pirate_mtk() =
     # PIRACY! But it should be benign.
     # I'd like to use `nothing`, but MTK doesn't like it
     @eval MTK (m::Model)(args...; name=:unnamed, kw...) = m.f(args...; name, kw...)
+
+function __init__()
+    pirate_mtk()
 end
 
 end
